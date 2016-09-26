bc2000245d58116d1393b0c77520d7c51c4e7eea
Scott Humphries <git@sscotth.io>
2016-09-26 10:56:25 -0500

    GET title from Node

--- client/app/main.js
+++ client/app/main.js
@@ -2,6 +2,10 @@
 
 angular
   .module('mean101', [])
-  .controller('main', function ($scope) {
-    $scope.title = 'MEAN 101'
+  .controller('main', function ($scope, $http) {
+    $http
+      .get('/api/title')
+      .then(({ data: { title }}) =>
+        $scope.title = title
+      )
   })
--- server/server.js
+++ server/server.js
@@ -7,4 +7,8 @@ const port = process.env.PORT || 3000
 
 app.use(express.static('client'))
 
+app.get('/api/title', (req, res) =>
+  res.json({ title: 'MEAN 101' })
+)
+
 app.listen(port, () => console.log(`Listening on port: ${port}`))
