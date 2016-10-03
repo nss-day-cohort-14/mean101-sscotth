5f09523fd067a24680042246c7b5d6f3396cf747
Scott Humphries <git@sscotth.io>
2016-10-03 10:20:21 -0500

    Initial websocket implementation

--- client/app/main.js
+++ client/app/main.js
@@ -1,5 +1,9 @@
 'use strict'
 
+const socket = io()
+socket.on('connect', () => console.log(`Socket connected: ${socket.id}`))
+socket.on('disconnect', () => console.log('Socket disconnected'))
+
 angular
   .module('mean101', ['ngRoute'])
   .config(($routeProvider, $locationProvider) => {
